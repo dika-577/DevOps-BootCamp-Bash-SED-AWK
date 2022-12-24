80a8c11072afe01a29a80f9448c96dd6aea18f89
dimitri <korgalidzed@gmail.com>
2022-12-25 00:10:15 +0400

    add code to sedawk.sh

--- sedawk.sh
+++ sedawk.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 # Put you code here
+orig_file="./passwd"
+dest_file="./passwd_new"
+ 
+ cp $orig_file $dest_file
+
+    awk  -F: 'BEGIN{OFS=":"}/saned/{$7="/bin/bash"};{print}' $dest_file > temp && mv temp $dest_file 
+      sed  -i '/^avahi/s,usr/sbin/nologin,bin/bash,' $dest_file
+      awk -F: 'BEGIN{OFS=":"}{print $1 ":" $3 ":" $5 ":" $7}' $dest_file  > temp && mv temp $dest_file 
+      sed  '/daemon/Id' $dest_file > temp && mv temp $dest_file
+      awk -F: '($2 % 2 == 0) { $2="/bin/zsh" } { print }'  $dest_file > temp && mv temp $dest_file 
